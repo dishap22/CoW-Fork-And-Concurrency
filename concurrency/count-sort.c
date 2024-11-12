3a5b29ba227e5db6b091d655e43be9406d171c90
Shruthi Bathini <shruthibathini2006@gmail.com>
2024-11-12 23:36:37 +0530

    some kinda threading

--- concurrency/count-sort.c
+++ concurrency/count-sort.c
@@ -1,69 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <pthread.h>
 
 #define MAX_FILES 100
-#define MAX_ID 100000
+#define MAXTHREADS 4
+#define ID_HASH_SIZE 1000000
+#define NAME_HASH_SIZE 10000000
 
-// Struct to hold file information
 typedef struct {
     char name[128];
     int id;
     char timestamp[20];
 } File;
 
+typedef struct {
+    File *files;
+    int left_bound;
+    int right_bound;
+    int *count_array;
+    int use_id;
+} ThreadData;
+
+unsigned int hash_string(const char *str) {
+    unsigned int hash = 0;
+    int constant = 31;
+    unsigned int power = 1;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        hash += str[i] * power;
+        power *= constant;
+    }
+
+    return hash;
+}
+
+void* count_function(void *arg) {
+    ThreadData *data = (ThreadData *)arg;
+    for (int i = data->left_bound; i < data->right_bound; i++) {
+        int key = data->use_id ? data->files[i].id : hash_string(data->files[i].name);
+        data->count_array[key] = i + 1;
+    }
+    return NULL;
+}
+
+void initialize_count_array(File files[], int num_files, int *count_array, int use_id) {
+    pthread_t threads[MAXTHREADS];
+    ThreadData thread_data[MAXTHREADS];
+    int range = num_files / MAXTHREADS;
+    for (int i = 0; i < MAXTHREADS; i++) {
+        thread_data[i].files = files;
+        thread_data[i].count_array = count_array;
+        thread_data[i].left_bound = i * range;
+        thread_data[i].right_bound = (i == MAXTHREADS - 1) ? num_files : (i + 1) * range;
+        thread_data[i].use_id = use_id;
+        pthread_create(&threads[i], NULL, count_function, &thread_data[i]);
+    }
+    for (int i = 0; i < MAXTHREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+void print_sorted_files(File files[], int *count_array, int hash_size) {
+    printf("name id timestamp\n");
+    for (int i = 0; i < hash_size; i++) {
+        if (count_array[i] != 0) {
+            int index = count_array[i] - 1;
+            printf("%s %d %s\n", files[index].name, files[index].id, files[index].timestamp);
+        }
+    }
+}
+
 void read_input(File files[], int *num_files, char *sort_column) {
-    fscanf(stdin, "%d", num_files); // Read number of files
+    fscanf(stdin, "%d", num_files);
     for (int i = 0; i < *num_files; i++) {
         fscanf(stdin, "%s %d %s", files[i].name, &files[i].id, files[i].timestamp);
     }
-    fscanf(stdin, "%s", sort_column); // Read the sort column
-}
-
-void counting_sort(File files[], int num_files, int sort_column, int *sorted_indices) {
-    int count[MAX_ID + 1] = {0}; // Initialize count array for IDs (range 0 to MAX_ID)
-    int output[num_files];
-
-    for (int i = 0; i < num_files; i++) {
-        count[files[i].id] = i + 1;
-    }
-
-    int index = 0;
-    for(int i = 0; i < MAX_ID + 1; i++) {
-        if(count[i] != 0) {
-            sorted_indices[index++] = count[i] - 1;
-        }
-    }
-    
-}
-
-void print_sorted_files(File files[], int num_files, int *sorted_indices, const char *sort_column) {
-    printf("%s\n", sort_column);
-    for (int i = 0; i < num_files; i++) {
-        int index = sorted_indices[i];
-        printf("%s %d %s\n", files[index].name, files[index].id, files[index].timestamp);
-    }
+    fscanf(stdin, "%s", sort_column);
 }
 
 int main() {
     File files[MAX_FILES];
+    int *count_array;
     int num_files;
     char sort_column[20];
 
-    // Read input data directly from stdin
     read_input(files, &num_files, sort_column);
+    int use_id = strcmp(sort_column, "ID") == 0;
 
-    // Array to hold the sorted indices
-    int sorted_indices[num_files];
+    int hash_size = use_id ? ID_HASH_SIZE : NAME_HASH_SIZE;
+    count_array = (int *)calloc(hash_size, sizeof(int));
 
-    // Perform distributed counting sort
-    counting_sort(files, num_files, 0, sorted_indices);
+    initialize_count_array(files, num_files, count_array, use_id);
+    print_sorted_files(files, count_array, hash_size);
 
-    // Print the sorted files based on the sorted indices
-    print_sorted_files(files, num_files, sorted_indices, sort_column);
-
+    free(count_array);
     return 0;
 }
